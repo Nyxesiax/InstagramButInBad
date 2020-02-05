48a94b8ad61f6724f5e5179bdae28dc073d8a395
Abdallah Taleb <abdallahtaleb96@yahoo.de>
2020-02-05 14:56:21 +0100

    Login Logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire/firebase.app.module';
-import { AngularFirestoreModule } from '@angular/fire/firestore/firestore.module';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { StartScreenComponent } from './components/start-screen/start-screen.component';
 import { LoginComponent } from './components/loginWindow/login.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuth, AngularFireAuthModule} from '@angular/fire/auth';
 
 const appRoutes: Routes = [
   { path: 'startScreen',
@@ -18,8 +20,8 @@ const appRoutes: Routes = [
     component: LoginComponent,
   },
   { path: '',
-    component: StartScreenComponent},//,
-  //{ path: '**', component: PageNotFoundComponent }
+    component: StartScreenComponent}, // ,
+  // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -38,6 +40,7 @@ const appRoutes: Routes = [
     AngularFirestoreModule,
     FormsModule,
     HttpClientModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/loginWindow/login.component.ts
+++ src/app/components/loginWindow/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../Service/authentication.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent  {
 
-  ngOnInit() {
+  constructor(
+    private authenticationService: AuthenticationService
+  ) {
+  }
+
+  email: string;
+  password: string;
+
+
+  signIn() {
+    this.authenticationService.SignIn(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+  signOut() {
+    this.authenticationService.SignOut();
   }
 
 }
